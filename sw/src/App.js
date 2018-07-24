3fe9e0a86fcf2390f0afea79188adba0d6dcb91d
Chris Harding <christopher.harding@gmx.com>
2018-07-23 20:29:44 -0400

    basic stopwatch functionality, creating child components for each split

--- sw/src/App.js
+++ sw/src/App.js
@@ -1,21 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      seconds: 55,
+      minutes: 0,
+      started: false,
+      splits: []
+    }
+    this.increment = null;
+    this.click = this.click.bind(this);
+    this.pause = this.pause.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  // https://stackoverflow.com/questions/35905988/react-js-how-to-append-a-component-on-clicks
+
+
+
+  click() {
+    if(this.state.started === false) {
+      this.setState({
+        started: true
+      })
+      this.increment = setInterval( () =>
+        this.setState({
+          seconds: this.state.seconds + 1,
+        })
+      , 1000);
+    }
+    else {
+      this.setState({
+        splits: this.state.splits.concat(<Split seconds={this.state.seconds} minutes={this.state.minutes} key={this.state.splits.length + 1} number={this.state.splits.length + 1}/>)
+      })
+    }
+
+  }
+
+  reset() {
+    clearInterval(this.increment)
+    this.setState({
+      seconds: 0
+    })
+  }
+
+  pause() {
+    clearInterval(this.increment)
+  }
+
+
+  componentDidUpdate() {
+    if(this.state.seconds === 60) {
+      this.setState({
+        seconds: 0,
+        minutes: this.state.minutes + 1
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.seconds < 10 ? <h1>{this.state.minutes}:0{this.state.seconds}</h1> : <h1>{this.state.minutes}:{this.state.seconds}</h1>}
+        <button onClick={this.click}>CLICK!</button>
+        <br/>
+        <br/>
+        {this.state.splits}
       </div>
     );
   }
+}
+
+function Split(props) {
+  if(props.seconds < 10) {
+    return (<div>
+      <button onClick={test(props.number)}>Hello, {props.minutes}:0{props.seconds}</button>
+      <br/>
+    </div>)
+  }
+  else {
+    return (<div>
+      <button onClick={test(props.number)}>Hello, {props.minutes}:{props.seconds}</button>
+      <br/>
+    </div>)
+  }
+}
+
+function test(input) {
+  console.log(input)
 }
 
 export default App;
---
+++ sw/src/Split.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+
+class Split extends Component {
+
+	render(){
+		return (
+      <div></div>
+		)
+	}
+
+}
+
+export default Split
